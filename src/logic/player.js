20aaee69bcf476f22930f2315e953dbee46ea622
Gabriel Davison <mail@gabrieldavison.me.uk>
2020-04-08 12:08:11 +0100

    Add module for player logic

---
+++ src/logic/player.js
@@ -0,0 +1,41 @@
+import {Gameboard, coordToIndex} from './gameboard'
+
+function randomCoord() {
+  return Math.floor(Math.random() * (9 +1))
+}
+let random = randomCoord
+
+function isLegal(board, coord) {
+  if (
+    board.board[coordToIndex(coord[0],coord[1])].hits === 'o'
+    || board.board[coordToIndex(coord[0],coord[1])].hits === 'x'
+   ) {return false}
+  return true
+}
+
+class Player {
+  constructor() {
+    this.gameboard = new Gameboard()
+    this.turns = []
+  }
+
+  takeTurn(x,y, board) {
+    board.recieveAttack(x,y);
+  }
+  // takes function to return random coord as a parameter
+  randomTurn(board, depRandomCoord) {
+    let coord = [depRandomCoord(), depRandomCoord()]
+    // wont play the same coord twice
+    while(!isLegal(board, coord)) {
+      coord = [depRandomCoord(), depRandomCoord()]
+    }
+    this.turns.push(coord)
+    board.recieveAttack(coord[0], coord[1])
+  }
+}
+
+
+
+
+
+export{Player, randomCoord, isLegal}
---
+++ src/logic/player.test.js
@@ -0,0 +1,48 @@
+import {Player, randomCoord, isLegal} from './player'
+
+
+const testPlayer1 = new Player();
+const testPlayer2 = new Player();
+
+test('player.taketurn attacks gameboard', () => {
+  testPlayer1.takeTurn(5,5, testPlayer2.gameboard)
+  expect(testPlayer2.gameboard.board[55].hits).toBe('o');
+})
+
+test('randomCoord generates random number between 0 and 9', () => {
+  for(let i = 0; i < 20; i++) {
+    expect((
+      randomCoord() >= 0 
+      && randomCoord() <= 9) 
+      && Number.isInteger(randomCoord()))
+    .toBe(true)
+  }
+})
+describe('tests using randomTurn', () => {
+  const mockRnd = jest.fn();
+
+  
+  test('randomTurn logs a random attack to board', () => {
+    mockRnd.mockReturnValueOnce(3).mockReturnValueOnce(4)
+    testPlayer2.randomTurn(testPlayer1.gameboard, mockRnd)
+    expect(testPlayer1.gameboard.board[43].hits).toBe('o');
+  })
+  
+  test('randomTurn logs its coordinates in turns array', () => {
+    expect(testPlayer2.turns[0]).toEqual([3,4])
+  })
+
+  test('isLegal returns false if space has already been attacked', () => {
+    expect(isLegal(testPlayer1.gameboard, [3,4])).toBe(false)
+  })
+
+  test('randomTurn wont play the same coordinates twice', () => {
+    mockRnd.mockReturnValueOnce(3).mockReturnValueOnce(4)
+    .mockReturnValueOnce(3).mockReturnValueOnce(4)
+    .mockReturnValueOnce(3).mockReturnValueOnce(4)
+  .mockReturnValueOnce(4).mockReturnValueOnce(5)
+    testPlayer2.randomTurn(testPlayer1.gameboard, mockRnd)
+    expect(testPlayer2.turns[1]).toEqual([4,5])
+  })
+})
+
